37372504487261ddcaaeadeffa1559ed0c843560
Mizanur Rahaman <mizanur15-8164@diu.edu.bd>
2021-01-25 04:44:08 +0600

    added login Ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage.page";
-import { BrowserRouter as Router } from "react-router-dom";
+
 import Shop from "./Pages/Shop/Shop.page";
 import { ROUTES } from "./util/ROUTES";
 import Header from "./Components/header/Header.component";
+import Authentication from "./Pages/Authentication/Authentication.component";
+
 function App() {
   return (
     <div>
       <Router>
         <Header />
         <Switch>
-          <Route exact to={ROUTES.ROOT} component={Shop} />
-          <Route exact to={ROUTES.SHOP} component={Shop}/>
+          <Route exact path={ROUTES.ROOT} component={HomePage} />
+          <Route exact path={ROUTES.SHOP} component={Shop}/>
+          <Route exact to={ROUTES.SIGN_IN} component={Authentication} />
         </Switch>
       </Router>
     </div>
---
+++ client/src/Components/SignIn/SignIn.component.tsx
@@ -0,0 +1,36 @@
+import React, {  useState } from "react";
+import CustomButton from "../custom-button/CustomButton.component";
+import FormInput from "../form-input/FormInput.component";
+import "./signIn.styles.scss";
+
+export default function SignIn() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className="sign-in">
+      <h2>I already Have an Account</h2>
+      <span>signIn With your Email and Password</span>
+      <form>
+        <FormInput
+          name="email"
+          type="email"
+          required={true}
+          value={email}
+          handleChange={(email:string) => setEmail(email)}
+          label="Email"
+        />
+        
+        <FormInput
+          name="password"
+          type="password"
+          required={true}
+          value={password}
+          handleChange={(password:string) => setPassword(password)}
+          label="Password"
+        />
+       <CustomButton type="submit">Sign In </CustomButton>
+      </form>
+    </div>
+  );
+}
