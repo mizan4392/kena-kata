d7dd411c6b15695df6c6d407265e022191922708
dev.mizan4392@gmail.com <dev.mizan4392@gmail.com>
2022-02-17 02:41:05 +0600

    added sidebar

---
+++ client/src/data/sidebarData.ts
@@ -0,0 +1,56 @@
+export const sidebarData = [
+  {
+    id: 1,
+    name: "cat1",
+    subCategories: [
+      {
+        id: 1,
+        name: "subcat1",
+      },
+      {
+        id: 2,
+        name: "subcat2",
+      },
+      {
+        id: 3,
+        name: "subcat3",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "cat2",
+    subCategories: [
+      {
+        id: 1,
+        name: "subcat2",
+      },
+      {
+        id: 2,
+        name: "subcat2",
+      },
+      {
+        id: 3,
+        name: "subcat3",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "cat3",
+    subCategories: [
+      {
+        id: 1,
+        name: "subcat3",
+      },
+      {
+        id: 2,
+        name: "subcat3",
+      },
+      {
+        id: 3,
+        name: "subcat3",
+      },
+    ],
+  },
+];
